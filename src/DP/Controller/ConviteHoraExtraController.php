886c5c876163480f17e151f3dff9c931dc03983e
IgorDePaula <principe.borodin@gmail.com>
2013-08-16 13:44:34 -0300

    Alteracao no convite de hora extra

--- src/DP/Controller/ConviteHoraExtraController.php
+++ src/DP/Controller/ConviteHoraExtraController.php
@@ -113,6 +113,7 @@ class ConviteHoraExtraController extends AbstractActionController {
             $mail = new MailService($this->getServiceLocator(), ServiceTemplate::DP_CONVITE_INDIVIDUAL_ROSE_APROVAR);
             $mail->addFrom('webmaster@irmserv.com.br')
                     ->addTo('prandini@irmserv.com.br')
+                    ->addCc($convite->getEmailsolicitante())
                     ->setSubject("[convite aprovado] Convite do dia {$convite->getDataregistro()}")
                     ->setBody(array('gerente' => $convite->getSupervisor(), 'aberto' => $convite->getDataregistro(), 'sujeito' => $convite->getNome(), 'inicio' => $convite->getDatainicio(), 'fim' => $convite->getDatafim(), 'motivo' => $convite->getMotivo()));
 
